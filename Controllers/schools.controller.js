8688a141607b94aeacc7928804534d4d2fa614e1
Dorian Legros <d.ratha76@hotmail.com>
2020-03-17 14:53:49 +0100

    improved possible responses with explicit messages and status

--- Controllers/schools.controller.js
+++ Controllers/schools.controller.js
@@ -2,7 +2,13 @@ const db = require("./../db");
 
 exports.get_all_schools = (req, res, next) => {
     db.query('SELECT * from schools', function (error, results, fields) {
-        if (error) throw error;
-        res.status(200).send(results);
+        if (error) {
+            res.status(500).send({status: "500", message: "Something went wrong, please verify if you're sending a valid request"});
+        }
+        else if (results.length === 0) {
+            res.status(404).send({status: "404", message: "Nothing found in the database"});
+        } else {
+            res.status(200).send(results);
+        }
     });
 };
